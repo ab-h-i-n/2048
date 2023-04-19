492e8556cb911a0dddc41c0f2dde4de38f748dd5
abhin_2122003 <abhinshelly@gmail.com>
2023-04-19 12:30:39 +0530

    swipe

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 
+//swipe
+var touchstartX = 0;
+var touchstartY = 0;
+var touchendX = 0;
+var touchendY = 0;
+
+var gesuredZone = document.getElementById('board');
+
+gesuredZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.screenX;
+    touchstartY = event.screenY;
+}, false);
+
+gesuredZone.addEventListener('touchend', function(event) {
+    touchendX = event.screenX;
+    touchendY = event.screenY;
+    handleGesure();
+}, false); 
+
+function handleGesure() {
+    var swiped = 'swiped: ';
+    if (touchendX < touchstartX) {
+        alert(swiped + 'left!');
+    }
+    if (touchendX > touchstartX) {
+        alert(swiped + 'right!');
+    }
+    if (touchendY < touchstartY) {
+        alert(swiped + 'down!');
+    }
+    if (touchendY > touchstartY) {
+        alert(swiped + 'left!');
+    }
+    if (touchendY == touchstartY) {
+        alert('tap!');
+    }
+}
+//key
 var board;
 var score = 90;
 var rows =4;
@@ -49,7 +87,7 @@ function updateTile (tile , num){
 }
 
 document.addEventListener("keyup",(e)=>{
-    if(e.code == "swiped-left"){
+    if(e.code == "ArrowLeft"){
         slideLeft();
     }
     else if(e.code == "ArrowRight"){
