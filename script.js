5a4760ef4bb6df0151ceb85c714b8245872fe527
abhin_2122003 <abhinshelly@gmail.com>
2023-04-19 16:13:10 +0530

    done

--- script.js
+++ script.js
@@ -4,24 +4,23 @@ var board;
 var score = 90;
 var rows =4;
 var columns =4;
+var reset = document.getElementById("reset");
 
 window.onload = function(){
     document.getElementById("date").innerHTML =new Date().getFullYear();
     setGame();
 }
 
+reset.addEventListener("click",()=>{
+    window.location.reload();
+});
+
 function setGame(){
-    // board = [
-    //     [0, 0, 0, 0],
-    //     [0, 0, 0, 0],
-    //     [0, 0, 0, 0],
-    //     [0, 0, 0, 0]
-    // ]
     board = [
-        [2, 2, 2, 2],
-        [2, 2, 2, 0],
-        [4, 4, 4, 16],
-        [8, 8, 8, 16]
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
     ]
 
     for(let i=0;i<rows;i++){
@@ -33,6 +32,8 @@ function setGame(){
             document.getElementById("board").append(tile);
         }
     }
+    setTwo();
+    setTwo();
 }
 
 function updateTile (tile , num){
@@ -47,20 +48,27 @@ function updateTile (tile , num){
             tile.classList.add("x2048")
         }
     }
+    if(checkWin()){
+        alert("You Win!");
+      }
 }
 
 document.addEventListener("keyup",(e)=>{
     if(e.code == "ArrowLeft"){
         slideLeft();
+        setTwo();
     }
     else if(e.code == "ArrowRight"){
         slideRight();
+        setTwo();
     }
     else if(e.code == "ArrowUp"){
         slideUp();
+        setTwo();
     }
     else if(e.code == "ArrowDown"){
         slideDown();
+        setTwo();
     }
 })
 function slideDown(){
@@ -144,4 +152,93 @@ return row;
 function filterZero(row){
     return row.filter(num => num!=0);
 }
+function hasEmptyTile(){
+    for(let i=0;i<rows;i++){
+        for(let j=0;j<columns;j++){
+            if(board[i][j] == 0){
+                return true;
+            }
+        }
+    }
+return false;
+}
 
+function setTwo(){
+
+    if(!hasEmptyTile()){
+        return;
+    }
+
+    let found = false;
+    while(!found){
+        let r =Math.floor(Math.random() * rows);
+        let c =Math.floor(Math.random() * columns);
+
+        if(board[r][c] == 0){
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            tile.innerText = "2";
+            tile.classList.add("x2");
+            found = true; 
+        }
+    }
+}
+
+function checkWin() {
+    for(let i=0;i<rows;i++){
+      for(let j=0;j<columns;j++){
+        if(board[i][j] == 2048){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+// Add touch event listeners
+var touchStartX, touchStartY, touchEndX, touchEndY;
+document.addEventListener("touchstart", function(event) {
+    touchStartX = event.changedTouches[0].clientX;
+    touchStartY = event.changedTouches[0].clientY;
+}, false);
+document.addEventListener("touchend", function(event) {
+    touchEndX = event.changedTouches[0].clientX;
+    touchEndY = event.changedTouches[0].clientY;
+    handleSwipe();
+}, false);
+
+function handleSwipe() {
+var swipeThreshold = 50; // minimum distance for a swipe
+var swipeDirection = "";
+
+var deltaX = touchEndX - touchStartX;
+var deltaY = touchEndY - touchStartY;
+if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    if (deltaX > swipeThreshold) {
+        swipeDirection = "right";
+    } else if (deltaX < -swipeThreshold) {
+        swipeDirection = "left";
+    }
+} else {
+    if (deltaY > swipeThreshold) {
+        swipeDirection = "down";
+    } else if (deltaY < -swipeThreshold) {
+        swipeDirection = "up";
+    }
+}
+
+// Call the corresponding function based on the direction of the swipe
+if (swipeDirection === "right") {
+    slideRight();
+    setTwo();
+} else if (swipeDirection === "left") {
+    slideLeft();
+    setTwo();
+} else if (swipeDirection === "down") {
+    slideDown();
+    setTwo();
+} else if (swipeDirection === "up") {
+    slideUp();
+    setTwo();
+}
+}
