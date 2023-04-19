077113672e918304c0a2cb7c7e0830b7e9352ae6
abhin_2122003 <abhinshelly@gmail.com>
2023-04-19 12:42:48 +0530

    no swipe

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
 
-//swipe
-var touchstartX = 0;
-var touchstartY = 0;
-var touchendX = 0;
-var touchendY = 0;
-
-var gesuredZone = document.getElementById('body');
-
-gesuredZone.addEventListener('touchstart', function(event) {
-    touchstartX = event.screenX;
-    touchstartY = event.screenY;
-}, false);
-
-gesuredZone.addEventListener('touchend', function(event) {
-    touchendX = event.screenX;
-    touchendY = event.screenY;
-    handleGesure();
-}, false); 
-
-function handleGesure() {
-    var swiped = 'swiped: ';
-    if (touchendX < touchstartX) {
-        alert(swiped + 'left!');
-    }
-    if (touchendX > touchstartX) {
-        alert(swiped + 'right!');
-    }
-    if (touchendY < touchstartY) {
-        alert(swiped + 'down!');
-    }
-    if (touchendY > touchstartY) {
-        alert(swiped + 'left!');
-    }
-    if (touchendY == touchstartY) {
-        alert('tap!');
-    }
-}
 //key
 var board;
 var score = 90;
